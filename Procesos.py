5a280dbb30879faf4b1742dd97a54bf7ade815e8
Sebas021210 <ssolorzano10@gmail.com>
2022-03-14 22:37:47 -0600

    Simulador de procesos

---
+++ Procesos.py
@@ -0,0 +1,50 @@
+'''
+Programa para la simulación  de  corrida  de  programas  en  un  sistema  operativo  
+de  tiempo  compartido.
+
+Programado por:  Sebastián Solorzano
+
+Fecha:  14 marzo de 2022
+'''
+
+# Importar funciones necesarias
+
+import simpy
+import random
+
+# Iniciar funciones
+
+def proceso(num, env, cpu, llegada):
+    global tiempo_final
+    yield env.timeout(llegada)
+    tiempo_inicial = env.now
+    inTime = random.randint(1, 10)
+    print ("%s tiempo inicial %f usa %d pasos out CPU" % (num, tiempo_inicial, inTime))
+
+    with cpu.request() as req:
+        yield req
+        yield env.timeout(inTime)
+        print ("%s libre al %f" % (num, env.now))
+
+    tiempo_final = env.now - tiempo_inicial
+    print ("%s el tiempo final es %f" % (num, tiempo_final))
+    tiempo_final = tiempo_final + tiempo_final
+
+random.seed(3)
+env = simpy.Environment() 
+initial_cpu = simpy.Resource(env, capacity=1)
+tiempo_final = 0
+initial_procesos = 50  
+
+#initial_ram = simpy.Container(env, 30, init=30)  # crea el container de la ram
+
+
+for i in range(initial_procesos):
+    llegada = 0 
+    #cantidad_instrucciones = random.randint(1, 10)  
+    #UsoRam = random.randint(1, 10)  
+    env.process(proceso('proceso %d' % i, env, initial_cpu, llegada))
+
+# correr la simulacion
+env.run(until = 500)
+print('tiempo promedio %d ' % (tiempo_final / initial_procesos))
